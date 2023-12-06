dbe77bf0d56f9e4b036f563eb1dfb2c6f1b541b3
Jonathan Rajao <hei.tovomihaja@gmail.com>
2023-12-06 11:56:50 +0300

    feat: finish account's CRU operations

---
+++ src/genericInterface/CrudOperations.java
@@ -0,0 +1,10 @@
+package genericInterface;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudOperations<T>{
+    List<T> findAll() throws SQLException;
+    List<T> saveAll(List<T> toSave) throws SQLException;
+    T save(T toSave) throws SQLException;
+}
